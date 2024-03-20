264b3de3d0aa8a003addbddc8f5632141e0bd688
TonyLu12138 <138006045+TonyLu12138@users.noreply.github.com>
2024-03-20 14:30:08 +0800

    调整修改节点类型方法位置

--- pkg/models/versatel/v1alpha1/linstor/node.go
+++ pkg/models/versatel/v1alpha1/linstor/node.go
@@ -63,3 +63,31 @@ func CreateNode(ctx context.Context, c *client.Client, name, ip, nodeType string
 func DeleteNode(ctx context.Context, c *client.Client, nodename string) error {
 	return c.Nodes.Delete(ctx, nodename)
 }
+
+// ModifyNode 修改节点类型
+func ModifyNode(ctx context.Context, c *client.Client, nodename, nodeType string) (string, error) {
+	// 获取节点信息
+	// fmt.Printf("执行修改节点类型方法\n")
+	node, err := c.Nodes.Get(ctx, nodename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// fmt.Printf("要修改的节点信息：%+v\n", node)
+	// fmt.Printf("node.Name: %s\n", node.Name)
+	// fmt.Printf("nodename: %s\n", nodename)
+	// fmt.Printf("nodeType: %s\n", nodeType)
+
+	// 构建包含修改信息的对象
+	nodeModify := client.NodeModify{
+		NodeType: nodeType,
+	}
+
+	// 调用 LINSTOR API 更新节点信息
+	err = c.Nodes.Modify(ctx, node.Name, nodeModify)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	successMessage := fmt.Sprintf("节点 %s 的类型已成功修改为 %s", nodename, nodeType)
+	return successMessage, err
+}
